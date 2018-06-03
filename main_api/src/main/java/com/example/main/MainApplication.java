64b1744fd28be7da945b9135cf7413043eaed6a2
남궁현 <nkh@encoredtech.com>
2018-06-04 00:20:11 +0900

    fix: change pom.xml

--- main_api/src/main/java/com/example/main/MainApplication.java
+++ main_api/src/main/java/com/example/main/MainApplication.java
@@ -4,8 +4,12 @@ import java.util.Collections;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class MainApplication {
 
 	public static void main(String[] args) {
